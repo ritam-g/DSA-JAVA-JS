ec57ea99171fad68cda98348be57297629597e46
ritam-g <ritammaty2006@gmail.com>
2025-12-27 12:02:01 +0530

    constructro can call it self

--- OOP'S PART-1/ClassConstuctor.java
+++ OOP'S PART-1/ClassConstuctor.java
@@ -3,6 +3,9 @@ public class ClassConstuctor {
     public static void main(String[] args) {
         Student s1=new Student("Ritam maty", 60, 90.2f);
         s1.showDetails();
+        System.out.println("-----------");
+        Student s2=new Student();
+        s2.showDetails();
     }
     static class Student{
         int roll;
@@ -12,6 +15,10 @@ public class ClassConstuctor {
             this.name=name;
             this.roll=roll;
             this.marks=marks;
+        }
+        Student(){
+            //! CONSTRUCTRUCTAR IS CALLING  ANOTHER CONSTRUCTORE  
+            this("rohit",12,22.2f);
         }
         public void showDetails(){
             System.out.println(this.name);
