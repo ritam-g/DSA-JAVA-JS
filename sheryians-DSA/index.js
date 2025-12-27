b56059f0576f2e4e42062f7521e093444ec43d88
ritam-g <ritammaty2006@gmail.com>
2025-12-27 16:39:04 +0530

    afternoon practice

--- sheryians-DSA/index.js
+++ sheryians-DSA/index.js
@@ -1,15 +1,2 @@
-let words="ck bk ak"
-let arr=words.split(" ")
-for (let i = 0; i < arr.length; i++) {
-    
-    // console.log(arr[i][0].charCodeAt());
-    for (let j = 0; j < arr.length-1-i; j++) {
-        if (arr[j][0].charCodeAt()> arr[j+1][0].charCodeAt()) {
-            let temp=arr[j]
-            arr[j]=arr[j+1]
-            arr[j+1]=temp
-        }
-        
-    }
-}
-console.log(arr.join(" ").toString());
+const set=new Set(["a", "b", "c"]);
+console.log(set.has("b"));
