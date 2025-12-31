0ef6680d10e13819e55a3982b0f15f922d156c0c
ritam-g <ritammaty2006@gmail.com>
2025-12-31 11:23:13 +0530

    oop4

---
+++ oops4/first/Func.java
@@ -0,0 +1,11 @@
+package oops4.first;
+
+public class Func {
+    
+    public void show(){
+        System.out.println("this is from FUnce");
+    }
+    protected void call(){
+        System.out.println("calling ");
+    }
+}
---
+++ oops4/snd/Main.java
@@ -0,0 +1,11 @@
+package oops4.snd;
+
+import oops4.first.Func;
+
+public class Main extends Func {
+    public static void main(String[] args) {
+        Func fn=new Func();
+        fn.show();
+        
+    }
+}
