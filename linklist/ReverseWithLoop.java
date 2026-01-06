89d47fb9f65126179588c2374c177e49c1ec265a
ritam-g <ritammaty2006@gmail.com>
2026-01-06 20:33:52 +0530

    reversing link list without recursion

---
+++ linklist/ReverseWithLoop.java
@@ -0,0 +1,73 @@
+
+    class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class ReverseWithLoop {
+
+    static Node head = null;
+    static Node tail = null;
+
+    public static void main(String[] args) {
+
+        // Create first node
+        Node n1 = new Node(10);
+        head = n1;
+        tail = n1;
+
+        // Add more nodes
+        Node n2 = new Node(20);
+        tail.next = n2;
+        tail = n2;
+
+        Node n3 = new Node(30);
+        tail.next = n3;
+        tail = n3;
+
+        Node n4 = new Node(40);
+        tail.next = n4;
+        tail = n4;
+        Node newNode=reverse(head);
+        // Traverse list
+        Node temp = newNode;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static Node reverse(Node head){
+        // if(node.next==null){
+        //     head=tail;
+        //     return;
+        // }
+        // reverse(node.next);
+        
+        
+        // node.next.next = node;
+        // node.next = null;
+
+        // // update tail only once (original head)
+        //     tail = node;
+        //! WITH OUT TAIL AND RECURSION
+        Node prev=null;
+        Node curr=head;
+        while (curr!=null) {
+            Node nextNode=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr= nextNode;
+            
+        }
+        return prev;
+
+    }
+}
+
+
