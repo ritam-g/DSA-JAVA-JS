eb86ae382432618c9a77f38b48368534053003c6
ritam-g <ritammaty2006@gmail.com>
2026-01-11 11:03:00 +0530

    practicing dsa

--- sheryians-DSA/DsaPractice.js
+++ sheryians-DSA/DsaPractice.js
@@ -1,24 +1,22 @@
-
-function findFactors(n,k) {
-    // Write your code here
-    let s=0
-    let c=0
-    return rec(s,n,c,k)
-    
-}
-function rec(s,n,c,k){
-    if(s>n) {
-        return -1
+function mergeSortedArrays(arr1, m, arr2, n) {
+        // implement logic
+        let i=m-1
+        let j=n-1
+        let len=m+n-1
+        while (i>=0&&j>=0) {
+            if (arr1[i]>arr2[j]) {
+                arr1[len]=arr1[i]
+                i--
+            }
+            else{
+                 arr1[len]=arr2[j]
+                j--
+            }
+            len--
+        }
+        for(let x=0;x<1;x++){
+            console.log('['+ arr1.join(',')+']')
+        }
+        
     }
-    if(n%s===0){
-        c++;
-        if(c==k) return s
-    }
-    return rec(s+1,n,c,k)
-    
-    
-}
-console.log(findFactors(6,3));
-
-
-
+    mergeSortedArrays([1,2,34,0],3,[1],1)
