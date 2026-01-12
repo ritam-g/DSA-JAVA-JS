7c29f22f58b170e84ac3ff0bd799179080e7f6d4
ritam-g <ritammaty2006@gmail.com>
2026-01-12 10:18:28 +0530

    making coustom tree

---
+++ tree/binarytree/BinaryTreeCousteme.java
@@ -0,0 +1,65 @@
+class tree {
+    int value;
+    tree left;
+    tree right;
+
+    tree(int val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BT {
+    tree root = null;
+
+    public void insert(int value) {
+        tree node = new tree(value);
+        if (root == null) {
+            root = node;
+            return; // IMPORTANT FIX
+        }
+        leftRight(root, node);
+    }
+
+    public tree leftRight(tree curr, tree node) {
+        if (curr == null) {
+            return node;
+        }
+
+        if (node.value < curr.value) {
+            curr.left = leftRight(curr.left, node);
+        } else if (node.value > curr.value) {
+            curr.right = leftRight(curr.right, node);
+        }
+        // duplicate values ignored
+
+        return curr;
+    }
+
+    public void display() {
+        inorder(root," ");
+    }
+
+    private void inorder(tree node, String indent) {
+    if (node == null) return;
+
+    System.out.println(indent + node.value);
+    inorder(node.left, indent + "L-- ");
+    inorder(node.right, indent + "R-- ");
+}
+
+}
+
+public class BinaryTreeCousteme {
+    public static void main(String[] args) {
+
+        BT binaryTree = new BT();
+        for (int i = 0; i < 10; i++) {
+            binaryTree.insert(i);
+            binaryTree.insert(i-10);
+        }
+
+        binaryTree.display(); // Output: 1 2 3
+    }
+}
