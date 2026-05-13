2a32820bec8ba21a52daec7f6838d274bb546412
ritam-g <ritammaty2006@gmail.com>
2026-05-13 16:49:42 +0530

    fix: update practice counts for Largest and Second Largest Element entries in service sheet

--- sheryians-DSA/practice.js
+++ sheryians-DSA/practice.js
@@ -1,13 +1,44 @@
 // Largest Element in Array – GFG    
 
-function findLargest(arr) {
-    if (arr.length <= 1) return
-    let max = arr[0]
-    arr.map((ele) => {
-        if(max<ele) max=ele
-    })
+// function findLargest(arr) {
+//     if (arr.length <= 1) return
+//     let max = arr[0]
+//     arr.map((ele) => {
+//         if(max<ele) max=ele
+//     })
 
-    return max
-}
+//     return max
+// }
 
-console.log(findLargest([1,20,3,4,5]));
+// console.log(findLargest([1,20,3,4,5]));
+
+
+// Second Largest Element – GFG	
+
+// function getSndLargest(arr) {
+//     if (arr.length <= 1) return
+
+//     let max = arr[0]
+//     let sndMax = -Infinity
+
+//     arr.map(ele => {
+//         if (ele > max) {
+//             if (max > sndMax && max < ele) {
+//                 sndMax = max
+//             }
+//             max = ele
+//         }
+//         if (ele > sndMax && ele < max) {
+//             sndMax = ele
+//         }
+//     })
+
+//     return sndMax
+
+// }
+
+
+// console.log(getSndLargest([1, 2, 3, 45, 6, 10]))
+
+
+// Check if Array is Sorted – GFG
